2968fcd71e8efcc703550ce8cc574d42667ed654
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-10-29 14:48:29 -0700

    Fixed header based on tutorial's  recommendation

--- src/TitleCard.jsx
+++ src/TitleCard.jsx
@@ -3,9 +3,10 @@ import trollFace from './images/troll-face.png'
 
 export function TitleCard() {
     return (
-        <div className="titleCardDiv" >
-            <img src={trollFace} />
-            <h1>Meme Generator</h1>
-        </div>
+        <header className="titleCardHeader" >
+            <img className="header--image" src={trollFace} />
+            <h1 className="header--h1">Meme Generator</h1>
+            <h4 className="header--h4">React Course - Project 3</h4>
+        </header>
     )
 }
